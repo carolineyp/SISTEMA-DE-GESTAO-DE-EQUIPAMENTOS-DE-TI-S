dfee530dfc448751ad1d5b74d4114a1c79d1ec5f
carolineyp <caroline.simao1300@gmail.com>
2025-09-13 15:13:01 -0300

    Corrigido login.html, atualizado redme.py e adicionado sla.py

--- redme.py
+++ redme.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template
+from flask import Flask as flask, render_template, request, redirect, url_for
 
 app = flask(__name__)
 
@@ -10,22 +10,40 @@ if __name__ == '__redme__':
     app.run()
 
 
+@app.route('/dologin', methods=['POST'])
+def dologin():
+    email = request.form['login']
+    password = request.form['senha']
+    # Aqui você pode adicionar a lógica de autenticação
+    if email == 'carol@gmail.com' and password == '1234':
+        return redirect(url_for('principal.html'))
+    else:
+        return "Credenciais inválidas, tente novamente ou recupere sua senha."
+if __name__ == '__redme__':
+    app.run(host="0.0.0.0", port=81)
+
+
+
+
+
+
+
 @app.route('/cadastro.html')
-def login():
-    return render_tamplate('cadastro.html')
+def cadastro():
+    return render_template('cadastro.html')
 if __name__ == '__redme__':
     app.run()
 
 
 @app.route('/recuperarsenha.html')
-def login():
-    return render_tamplate('recuperarsenha.html')
+def recuperar_senha():
+    return render_template('recuperarsenha.html')
 if __name__ == '__redme__':
     app.run()  
 
 
- @app.route('/principal.html')
-def login():
-    return render_tamplate('principal.html')
+@app.route('/principal.html')
+def principal():
+    return render_template('principal.html')
 if __name__ == '__redme__':
     app.run()
